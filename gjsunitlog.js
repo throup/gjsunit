e748ee3d0327be8c6ee45e4d9298b950bd06c761
Chris Throup <chris@throup.org.uk>
2014-06-24 22:01:11 +0100

    Extracted domain class definitions into separate source file.

--- gjsunitlog.js
+++ gjsunitlog.js
@@ -5,56 +5,10 @@ imports.searchPath.push('.');
 imports.searchPath.push('/usr/share/gnome-js');
 imports.searchPath.push('/usr/share/gnome-shell/js');
 
+const Testsuite = imports.src.domain.Testsuite;
+const Testcase = imports.src.domain.Testcase;
+
 var testSuites = [];
-
-function Testsuite(name) {
-	this._init(name);
-}
-Testsuite.prototype = {
-	_init: function(name) {
-		this.name = name;
-		this.cases = [];
-		this.countTestsOverall = 0;
-		this.countTestsFailed = 0;
-	},
-
-	log: function() {
-		let output = '<testsuite name="' + this.name + '" tests="' + this.countTestsOverall + '" failures="' + this.countTestsFailed + '">';
-		for( let i in this.cases ) {
-			let tc = this.cases[i];
-			if( tc.log ) {
-				output += tc.log();
-			}
-		}
-		output += '</testsuite>';
-		return output;
-	}
-}
-
-function Testcase(name) {
-	this._init(name);
-}
-
-Testcase.prototype = {
-	_init: function(name) {
-		this.name = name;
-		this.error = '';
-	},
-
-	log: function() {
-		let output = '<testcase name="' + this.name + '"';
-		if( !this.error ) {
-			output += '/>';
-		} else {
-			output += '><failure message="' + this.error + '"/></testcase>';
-		}
-		return output;
-	},
-
-	fail: function(message) {
-		this.error = message;
-	}
-}
 
 window.describe = function(moduleName, callback) {
 	var inner = new Testsuite(moduleName);
